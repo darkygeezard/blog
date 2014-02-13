5bf65d14cb80927434be2a3cd6ffde6c0a739ef0
DarKy Geezard <darky geezard@mail.com>
2014-02-13 11:03:47 +0300

    Final guides changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
+  
+  resources :posts do
+    resources :comments
+  end
+
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
